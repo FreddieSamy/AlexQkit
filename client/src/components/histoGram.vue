b0bf0210b3a14ef3bc1d39624c453a8cb5a0832e
Mario <mario3monir@gmail.com>
2020-03-01 17:17:09 +0200

    delete unused functions in wire and clone components

--- client/src/components/histoGram.vue
+++ client/src/components/histoGram.vue
@@ -8,18 +8,26 @@
       />
   </div>
 </template>
+<!-- ========================================================== -->
+<script>
+export default {
+}
+</script>
+<!-- ========================================================== -->
 <style scoped>
 .histoGram {
   flex:1;
-  border: 0.2em solid black;
+  border: 0.05em solid black;
   padding: 0em 0em 0em 0em;
-  margin: 0em 1em 1em 1em;
+  margin: 0em 0em 0em 1em;
   border-radius: 0.5em;
-  width:45%;
+
 }
 .histogram-image{ 
-  margin: 0em 0em 0em 0em;
+  margin: 4em 0em 0em 0em;
   padding: 0em 0em 0em 0em;
-  width:38em; 
+  height:auto;
+  width: auto;
+  max-width:35em;
 }
 </style>
