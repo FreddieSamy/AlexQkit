977d28a90d192fdaa3cba9d86b81477b2e1347ec
muhamedhassan007 <56801636+muhamedhassan007@users.noreply.github.com>
2020-03-02 01:14:04 +0200

    update ibm.vue
    alots of love

--- client/src/components/ibm.vue
+++ client/src/components/ibm.vue
@@ -1,30 +1,57 @@
 <template>
   <div class="ibmBody">
     <label class="lbl1">IBM Token</label>
-    <input class="ibmToken" type="text" name="" placeholder="ibmToken (not working yet)" id="" />
+    <input class="ibmtoken"  type="text" id="ibmtextfield"/>
+    <!-- <div>
+    <label class="lbl1">number of shots</label>
+    <input class="ibmToken" type="text" />
+    </div> -->
+    <div>
+      <button @click="get_ibm_token()">save</button>
+    </div>
+    <!-- <div>
+    <label class="lbl1">simulator</label>
+    <input class="ibmToken" type="text"/>
+    </div> -->
   </div>
+  
+
 </template>
 <!-- =============================================================  -->
 <script>
 export default {
   name: "ibm",
   display: "ibm",
-  order: 3
+  order: 3,
+  methods:{
+    get_ibm_token(){
+    var ibmtext = document.getElementById("ibmtextfield").value;
+    //window.console.log(ibmtext);
+    return ibmtext;
+    
+    }
+
+  
+}
+
 };
+
 </script>
 <!-- =============================================================  -->
 <style scoped>
 .ibmBody {
-  flex: 1;
+  /*border: 2px dashed grey;*/
   padding: 0.2em 0.2em 0.2em 0.2em;
   margin: 0.2em 0.2em 0.2em 0.2em;
   width: 29%;
+  float: right;
+  
 }
 .lbl1{
-  display:block;
+  display:inline-block;
 }
 .ibmToken{
-  display:block;
+  display:inline-block;
   margin: 0.5em 0.2em 0.2em 0.2em;
 }
 </style>
