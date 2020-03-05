a2e2d9e24bf5ea85068da3da6c58dd6d62523147
Mario <mario3monir@gmail.com>
2020-03-05 16:52:51 +0200

    un-stable-version

--- client/src/components/wire.vue
+++ client/src/components/wire.vue
@@ -13,7 +13,7 @@
       @add="add"
       @remove="remove"
       @update="update"
-      @onEnd="change"
+      @change="change"
     >
       <div
         class="circuit-gate"
@@ -47,20 +47,14 @@ export default {
     //-----------------------------------------------------------------------
     add:function(evt){
       var gate = evt.to.childNodes[evt.newIndex];
-      gate.setAttribute('row',this.id);
-      gate.setAttribute('col',evt.newIndex+1);
-      //window.console.log(gate);
-      //window.console.log(gate);
-      //window.console.log(evt.item.innerHTML);
-      //window.console.log("happen add");
-      //window.console.log("enter on end")
-      //window.console.log("item id : "+evt.item.id);
-      //window.console.log("to      : "+evt.to.classList.value);
-      //window.console.log(evt.from.childNodes);
-      //window.console.log("old i   : "+evt.oldIndex);
-      //window.console.log("new i   : "+evt.newIndex);
-      //window.console.log(evt.clone);
-      //window.console.log(evt.pullMode);
+      var gateName = gate.id;
+      var col = "_"+this.id;
+      var row = "_"+(evt.newIndex+1);
+      gate.setAttribute('row',row);
+      gate.setAttribute('col',col);
+      if(gateName=="c"){
+          this.$parent.applyControl(col);
+      }
       this.$parent.updateMaxWire(); 
       this.$parent.addIdentityToColumn(this.id);
       this.$parent.removeIdentitySystem();
@@ -73,8 +67,8 @@ export default {
     },
     update:function(evt){
       var gate = evt.to.childNodes[evt.newIndex];
-      gate.setAttribute('row',this.id);
-      gate.setAttribute('col',evt.newIndex+1);
+      window.console.log(gate);
+      window.console.log(gate.innerText);
       window.console.log(gate);
       /*
       window.console.log("enter on update")
@@ -107,8 +101,8 @@ export default {
       this.$parent.exeCount = this.$parent.maxWire;
       //this.$parent.updateTracingLine();
     },
-    change: function(/*event,evt*/) {
-       window.console.log("happen change");
+    change: function(/*evt*/) {
+       //window.console.log('change '+evt.newIndex);
       /*
       window.console.log("enter on change")
       this.$parent.updateMaxWire(); // update wire that has maximum gates
@@ -207,15 +201,6 @@ export default {
         this.list.push({ name: gatesList[colIdx] });
       }
     },
-    //-----------------------------------------------------------------------
-    objectNames: function(listOfObject) {
-      // unused till now
-      var names = [];
-      for (let i = 0; i < listOfObject.length; i++) {
-        names.push(listOfObject[i]["name"]);
-      }
-      return names;
-    }
     //-----------------------------------------------------------------------
   }
 };
