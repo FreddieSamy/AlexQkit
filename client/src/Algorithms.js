aa71068e7052dd12bed6697021a5f67cd53be2c5
Mario <mario3monir@gmail.com>
2020-03-20 18:01:30 +0200

    push Algorithms.js

---
+++ client/src/Algorithms.js
@@ -0,0 +1,50 @@
+export const Algorithms = [
+  {
+    name: "tele_algorithm1",
+    circuit: {
+      wires: 3,
+      init: ["0", "0", "0"],
+      rows: [
+        ["x", "i", "c", "h", "i", "h"],
+        ["i", "h", "c", "x", "c", "i"],
+        ["i", "i", "x", "i", "h", "c"],
+      ],
+    },
+  },
+  {
+    name: "tele_algorithm2",
+    circuit: {
+      wires: 3,
+      init: ["0", "0", "0"],
+      rows: [
+        ["x", "i", "c", "x", "i", "h"],
+        ["x", "h", "c", "x", "c", "i"],
+        ["x", "i", "x", "x", "h", "c"],
+      ],
+    },
+  },
+  {
+    name: "tele_algorithm3",
+    circuit: {
+      wires: 3,
+      init: ["0", "0", "0"],
+      rows: [
+        ["x", "i", "c", "h", "x", "h"],
+        ["i", "x", "c", "x", "x", "i"],
+        ["i", "i", "x", "i", "x", "c"],
+      ],
+    },
+  },
+  {
+    name: "tele_algorithm4",
+    circuit: {
+      wires: 3,
+      init: ["0", "0", "0"],
+      rows: [
+        ["x", "x", "x", "x", "x", "x"],
+        ["i", "h", "c", "x", "c", "x"],
+        ["i", "i", "x", "i", "h", "x"],
+      ],
+    },
+  },
+];
