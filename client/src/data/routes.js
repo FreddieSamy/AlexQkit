26a422f7872fbe3bb3e07b5cdd7c575682c03290
Mario <mario3monir@gmail.com>
2020-04-09 20:00:17 +0200

    add vuex , data , store

---
+++ client/src/data/routes.js
@@ -0,0 +1,14 @@
+// we should put all routes here
+
+export default {
+    serverRoute      : "http://localhost:5000/",
+    appRoute         : "http://localhost:5000/data", 
+    resetRoute       : "http://localhost:5000/reset",
+    histogramRoute   : "http://127.0.0.1:5000/chart.png?time",
+    blockSphereRoute : "http://127.0.0.1:5000/blochsphere.png?time=",
+    qasmCircuitRoute : "http://127.0.0.1:5000/circuit.png?time=",
+    elementaryGates  : "http://localhost:5000/elementaryGates",
+    customGates      : "http://localhost:5000/subCircuitCustomGate",
+    
+}
+
---
+++ client/src/store/state.js
@@ -0,0 +1,12 @@
+import jsonObject from './../data/jsonObject.js'
+import algorithms from './../data/algorithms.js'
+import routes from './../data/routes.js'
+import states from './../data/states.js'
+
+
+export default{
+jsonObject,
+algorithms,
+routes,
+states
+}
