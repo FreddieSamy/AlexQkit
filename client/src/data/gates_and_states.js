6c76bb784ee5a33177f70b5962e3f080262d7835
Mario <mario3monir@gmail.com>
2020-04-16 19:47:25 +0200

    add a prototype for add loop feature

--- client/src/data/gates_and_states.js
+++ client/src/data/gates_and_states.js
@@ -2,10 +2,6 @@
 export const states =  ["0", "1", "+", "-", "i", "-i"] ;
 
 export const gates = [
-    { name: "c", id: "c", index: "", info: "closed control" },
-    { name: "m", id: "m", index: "", info: "measurment gate" },
-    { name: "oc", id: "oc", index: "", info: "open control" },
-    { name: "reset", id: "reset", index: "", info: "reset gate" },
     { name: "x", id: "x", index: "", info: "not gate" },
     { name: "y", id: "y", index: "", info: "" },
     { name: "z", id: "z", index: "", info: "" },
@@ -13,6 +9,10 @@ export const gates = [
     { name: "swap", id: "swap", index: "", info: "" },
     { name: "s", id: "s", index: "", info: "" },
     { name: "t", id: "t", index: "", info: "" },
+    { name: "c", id: "c", index: "", info: "closed control" },
+    { name: "m", id: "m", index: "", info: "measurment gate" },
+    { name: "oc", id: "oc", index: "", info: "open control" },
+    { name: "reset", id: "reset", index: "", info: "reset gate" },
     { name: "sdg", id: "sdg", index: "", info: "" },
     { name: "tdg", id: "tdg", index: "", info: "" },
     { name: "rx", id: "rx", index: "", info: "" },
