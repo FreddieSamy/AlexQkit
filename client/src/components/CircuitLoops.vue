9f5e94ce64a0aa72611bf57daa000c11f6b63b4f
Mario <mario3monir@gmail.com>
2020-04-17 00:54:33 +0200

    add file circiutloop , loop . vue

---
+++ client/src/components/CircuitLoops.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="circiut-loops">
+    <div>
+          <button @click="loopCounts++">Add Loop</button>
+    </div>
+
+    <loop v-for="count in loopCounts" :key="count" :ref="'loops'" ></loop>
+
+    <div>
+      <button v-if="loopCounts" @click="applyLoop">Apply Loops</button>
+    </div>
+    <div>
+      <button v-if="loopCounts" @click="loopCounts--">Remove Loop</button>
+    </div>
+
+  </div>
+</template>
+<!-- ================================================  -->
+<script>
+import loop from "./loop.vue";
+// eslint-disable-next-line no-undef
+export default {
+  name: "CircuitLoops",
+  display: "CircuitLoops",
+  props: ["maxWire"],
+  components:{loop},
+  data() {
+    return {
+      loopCounts:0,
+      ListOfPositions:[],
+      Repeats:[]
+    };
+  },
+
+  computed: {
+    
+  },
+  methods: {
+    applyLoop(){
+      this.ListOfPositions=[]
+      this.Repeats=[]
+      for (let i = 0; i < this.loopCounts; i++) {
+        let loopCaller = this.$refs.loops[i];
+        let arr = [ loopCaller.from, loopCaller.to ]
+        this.ListOfPositions.push(arr);
+        this.Repeats.push(loopCaller.repeat)
+      }
+
+    }
+  }
+};
+</script>
+<!-- ================================================  -->
+<style scoped>
+/*
+@import url("./../../node_modules/nouislider/distribute/nouislider.min.css");
+*/
+
+button{
+  border-radius: 7px;
+  background-color: white;
+  margin:0em 0em 0.5em 0em;
+}
+
+</style>
+
+
---
+++ client/src/components/loop.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div>
+      <input type ="number" v-model="from" placeholder="from"/>
+      <label> : </label>
+      <input type="number"  v-model="to" placeholder="to" />
+       <label> -> </label>
+      <input type="number"  v-model="repeat" placeholder="Repeat" />
+    </div>
+  
+  </div>
+</template>
+<!-- ================================================  -->
+<script>
+// eslint-disable-next-line no-undef
+export default {
+  name: "loop",
+  display: "loop",
+  //props: ["maxWire"],
+  data() {
+    return {
+      from:'from',
+      to:'to',
+      repeat:'repeat'
+    };
+  },
+
+  computed: {},
+  methods: {}
+};
+</script>
+<!-- ================================================  -->
+<style scoped>
+input{
+  padding: 0em 0em 0em 0.5em;
+  width:60px;
+  border-radius: 5px;;
+}
+</style>
+
