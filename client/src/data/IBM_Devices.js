6bb6ce13868e5e5adf11cec96a9b781900bc4d93
Mario <mario3monir@gmail.com>
2020-04-17 03:34:12 +0200

    add ibm devices file

---
+++ client/src/data/IBM_Devices.js
@@ -0,0 +1,15 @@
+export default {
+  "1 qubit": ["IBMQ_armonk"],
+  "5 qubits": [
+    "IBMQ_london",
+    "IBMQ_burlington",
+    "IBMQ_essex",
+    "IBMQ_ourense",
+    "IBMQ_vigo",
+    "IBMQ_5_yorktown",
+  ],
+  "15 qubits": ["IBMQ_16_melbourne"],
+  "up to 32 qubits" : ["IBMQ_qasm_simulator"]
+};
+
+
