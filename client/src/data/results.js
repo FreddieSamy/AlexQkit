00622321cc1474711b86fcf8eab94784e903c820
Mario <mario3monir@gmail.com>
2020-04-28 19:13:59 +0200

    add results state management and git rid off unused functions

---
+++ client/src/data/results.js
@@ -0,0 +1,18 @@
+export default {
+    chart:[
+        ["Qbit", "state"],
+        ["000",  0.49267578125,],
+        ["001", 0.0],
+        ["010",  0.50732421875,],
+        ["011", 0],
+        ["100", 0],
+        ["101", 0.8],
+        ["110", 0],
+        ["111", 0]
+      ],
+diracNotation: " |00⟩ ",
+link: "",
+matrixRepresentation: [["1", "0", "0", "0"]],
+qasm: "OPENQASM 2.0;↵include 'qelib1.inc';↵qreg q[2];↵creg c[2];↵",
+qasmError: ""
+}
