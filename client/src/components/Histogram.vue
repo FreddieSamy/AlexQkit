f9d6c54819ef960778cc11cbfad9a33c0dcdceb0
Mario <mario3monir@gmail.com>
2020-04-29 05:26:07 +0200

    the histogram is working

--- client/src/components/Histogram.vue
+++ client/src/components/Histogram.vue
@@ -1,32 +1,30 @@
 <template>
   <div class="histogram">
-    <GChart style="width:500px" type="ColumnChart" :data="chartData" :options="chartOptions" />
+    <GChart style="width:500px" type="ColumnChart" :data="chart" :options="chartOptions" />
   </div>
 </template>
 <!-- ========================================================== -->
 <script>
 import { GChart } from "vue-google-charts";
+import { mapGetters } from "vuex"
 export default {
   name:'Histogram',
   display:'Histogram',
   components:{ GChart },
+  computed: {
+    ...mapGetters(['liveResults']),
+    chart(){
+        let temp = this.liveResults.chart;
+        temp.unshift( ["Qbit", "state"])
+        return temp;
+    }
+  },
   data() {
     return {
- chartData: [
-        ["Qbit", "state"],
-        ["000",  0.49267578125,],
-        ["001", 0.0],
-        ["010",  0.50732421875,],
-        ["011", 0],
-        ["100", 0],
-        ["101", 0.8],
-        ["110", 0],
-        ["111", 0]
-      ],
       chartOptions: {
         chart: {
-          title: "Company Performance",
-          subtitle: "Sales, Expenses, and Profit: 2014-2017"
+          title: "",
+          subtitle: ""
         },vAxis: {
             maxValue: 1,
           }
