62b2c68d924a4d9b61b0430b96e3362e4efa10bd
Mario <mario3monir@gmail.com>
2020-04-29 10:31:52 +0200

    a nice beta version

--- client/src/components/MatrixRepresentation.vue
+++ client/src/components/MatrixRepresentation.vue
@@ -1,58 +1,73 @@
 <template>
   <div id="matrixRepresentation" class="matrix-box">
-    <label>Matrix Representation</label>
-    <!--<katex-element  expression="\left(\begin{matrix}1 & 0 \\0 & 1\end{matrix}\right)"/>-->
-    <ul>
-      <li v-for="(i , row ) in this.liveResults.matrixRepresentation" :key="row">{{i}}</li>
-    </ul>
+    <label class="matrix-label">Matrix Representation</label>
+    <katex-element class="matrix"  :expression=matrixLatex />-
   </div>
 </template>
 <!-- =============================================================  -->
 
 <script>
 //"\left(\begin{matrix}1 & 0 \\0 & 1\end{matrix}\right)" // \begin{pmatrix}1&2 \\ 3&4\end{pmatrix}
+
+//\left(\begin{matrix}1 &0 \\0 & 1\\0&1\end{matrix}\right)
+//\left(\begin{matrix}1&0&0&0\\0&1&0&0\\0&0&1&0\\0&0&0&1\\end{matrix}\right)
 import Vue from "vue";
 import "katex/dist/katex.min.css"; // eh dah .... ?
 import VueKatex from "vue-katex";
 import { mapGetters } from "vuex";
+
 Vue.use(VueKatex, {
   globalOptions: {
-    //... Define globally applied KaTeX options here
+    
   }
 });
 
 export default {
   name: "MatrixRepresentation",
   display: "MatrixRepresentation",
+  data() {
+    return {
+    
+    }
+  },
   computed: {
-    ...mapGetters(["liveResults"])
-  }
+    ...mapGetters(["liveResults"]),
+      matrixLatex(){
+      // eslint-disable-next-line no-useless-escape
+      let prefix = "\\left(\\begin{matrix}"
+      let postfix = "end{matrix}\\right)"
+      var matlatex = ""
+      var matrix = this.liveResults.matrixRepresentation
+      for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix[0].length; j++) {
+                matlatex += matrix[i][j]+"&"
+              
+            }
+            matlatex = matlatex.slice(0, -1);
+             matlatex += "\\\\"
+            
+      }
+      matlatex = matlatex.slice(0, -1);
+      //window.console.log(prefix+matlatex+postfix)
+      return prefix+matlatex+postfix
+    }
+
+  },
 };
 </script>
 <!-- =============================================================  -->
 
 <style scoped>
-ul {
-  list-style-type: none;
-  flex-basis: 100%;
-  align-self: center;
-  padding: 0px;
-}
-li {
-  padding: 10px;
-}
-label {
-  flex-basis: 100%;
-  margin: 10px 0px 0px 0px;
-  align-self: center;
-}
 .matrix-box {
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: stretch;
+  align-items: center;
   padding: 0px;
   margin: 0px;
   border-radius: 0.5em;
+}
+.matrix-label{
+  margin: 0px 0px 20px 0px;
 }
 </style>
