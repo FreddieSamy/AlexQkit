78dd2ab0a033d719eb708afac8d14ead6fe262d7
Mario <mario3monir@gmail.com>
2020-05-05 17:21:13 +0200

    blochSphere

---
+++ client/src/components/BlochSphere.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <img src="../assets/b.png" alt="">
+  </div>
+</template>
+<!-- =============================================================  -->
+<script>
+
+export default {
+  name: "BlochSphere",
+  display: "BlochSphere",
+  components: {},
+  //props: ["percent"],
+  data(){
+     return {   
+         
+     }
+  },
+  computed:{
+  }
+};
+</script>
+<!-- =============================================================  -->
+<style scoped>
+div {
+
+
+  display: flex;
+  transition: transform .2s;
+  z-index: 0;
+}
+img {
+  width:50px;
+}
+
+div:hover{
+  transform: scale(3);
+  z-index: 2;
+}
+svg{
+    text-align: center;
+}
+</style>
---
+++ client/src/components/Probability.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <svg width="30" height="30">
+        <rect
+          width="30"
+          height="30"
+          x="0"
+          y="0"
+          stroke="black"
+          stroke-width="0.5"
+          fill="white"
+          rx="1"
+        />
+        <rect width="30" :height=percent x="0" :y=yAxis fill="lightgreen" rx="0" />
+        <text x="50%" y="18" text-anchor="middle" font-size="10" v-text="percent+'%'">%</text>
+    </svg>
+  </div>
+</template>
+<!-- =============================================================  -->
+<script>
+
+export default {
+  name: "Probability",
+  display: "Probability",
+  components: {},
+  props: ["percent"],
+  data(){
+     return {   
+         
+     }
+  },
+  computed:{
+    yAxis() {
+        return (100 - this.percent)+"%"
+    }
+  }
+
+};
+</script>
+<!-- =============================================================  -->
+<style scoped>
+div {
+  display: flex;
+  transition: transform .2s;
+  z-index: 0;
+}
+
+div:hover{
+  transform: scale(2);
+  z-index: 2;
+}
+svg{
+    text-align: center;
+}
+</style>
