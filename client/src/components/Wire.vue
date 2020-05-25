f1aee4c52905c17b7555b8abf2a7554595d7f278
FreddieSamy <Sci.FreddieSamy.2017@alexu.edu.eg>
2020-05-25 02:49:22 +0200

    delete any wire function

--- client/src/components/Wire.vue
+++ client/src/components/Wire.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="wire" :id="'wire-' + id">
-    <!--
     <div class="delete-wire" :id="'d-' + id">
       <button class="delete" @click="deleteWire">x</button>
     </div>
-    -->
 
-    <label class="qubit-name">q<sub>{{id-1}}</sub></label>
+    <label class="qubit-name">
+      q
+      <sub>{{id-1}}</sub>
+    </label>
 
     <!-- <div class="qubit"> -->
     <button class="qubit-state" :id="'q' + id + '-0'" @click="qubitState">|{{ state }}⟩</button>
@@ -56,7 +57,6 @@ export default {
     this.setGatesIdentity();
   },
   updated() {
-    // why this
     let wire = {
       qstate: this.state,
       list: this.getGates(this.id - 1),
@@ -75,7 +75,7 @@ export default {
   data() {
     return {
       list: [],
-      state: states[0],
+      state: states[0]
     };
   },
   watch: {
@@ -120,7 +120,7 @@ export default {
     //-----------------------------------------------------------------------
     updateWireAttributes: function() {
       // should be optimzed more
-      window.console.log("update Wire "+this.id+" Attributes");
+      window.console.log("update Wire " + this.id + " Attributes");
       let wire = document.querySelector("#list" + this.id + "");
       if (this.list.length) {
         let gates = wire.childNodes;
@@ -142,9 +142,12 @@ export default {
       this.runCircuit();
     },
     //-----------------------------------------------------------------------
-    deleteWire: function(evt) { // not used yet 
-      var el = evt.target.parentNode.parentNode;
-      el.parentNode.removeChild(el);
+    deleteWire: function() {
+      this.jsonObject.rows.splice(this.id - 1, 1);
+      this.jsonObject.init.splice(this.id - 1, 1);
+      this.jsonObject.wires--;
+      window.console.log(this.jsonObject);
+      this.$parent.setAlgorithm(this.jsonObject, false);
     },
     //-----------------------------------------------------------------------
     addIdentity: function() {
@@ -202,7 +205,7 @@ export default {
       if (append == false) {
         this.list = []; // disabled to push on the wire
       }
-      for (let colIdx = 0; colIdx < gatesList.length; colIdx++) {  // can be optimized more
+      for (let colIdx = 0; colIdx < gatesList.length; colIdx++) {
         this.list.push({ name: gatesList[colIdx] });
       }
     },
@@ -218,13 +221,13 @@ export default {
     },
     //-----------------------------------------------------------------------
     //should be put in vuex because we need it in toolbox (or could be terminated)
-    displayName: function(name) {    
+    displayName: function(name) {
       if (name.startsWith("custom_")) {
         return name.slice(7).toUpperCase();
       } else {
         return name.toUpperCase();
       }
-    },
+    }
     //-----------------------------------------------------------------------
   }
 };
@@ -248,8 +251,8 @@ export default {
 }
 
 .qubit-name {
-  margin:0px 5px 0px -5px;
-  padding:0px 5px 0px 0px;
+  margin: 0px 5px 0px -5px;
+  padding: 0px 5px 0px 0px;
   background: white;
   justify-self: center;
   align-self: center;
@@ -259,6 +262,7 @@ export default {
   border-radius: 0.5em;
   width: 2.5em;
   height: 2em;
+  margin-right: 0.6em;
   background-color: white;
   border: 2px solid #306ba2;
 }
