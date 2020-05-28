ec280da2a6c91d6f9860861c26a6ac253c58c470
FreddieSamy <Sci.FreddieSamy.2017@alexu.edu.eg>
2020-05-28 10:07:55 +0200

    CircuitBlock component

---
+++ client/src/components/CircuitBlock.vue
@@ -0,0 +1,109 @@
+<template>
+  <div id="circuit-block">
+    <label class="label">{{label}}</label>
+  </div>
+</template>
+<!-- ========================================================== -->
+<script>
+// import { getElementWidth } from "./tracingLine.vue";
+
+export default {
+  name: "circuitBlock",
+  // data() {
+  //   return {
+  //     label: "algorithm",
+  //     fromColumn: 5,
+  //     toColumn: 10
+  //   };
+  // },
+  props: ["label", "fromColumn", "toColumn"],
+  methods: {
+    // ...["getElementWidth"],
+    getElementWidth: function(className) {
+      if (document.getElementsByClassName(className)[0] != undefined) {
+        var ele = document.getElementsByClassName(className)[0];
+        var style = ele.currentStyle || window.getComputedStyle(ele);
+        return (
+          document.getElementsByClassName(className)[0].offsetWidth +
+          parseFloat(style.marginLeft) +
+          parseFloat(style.marginRight)
+        );
+      }
+      return 0;
+    },
+    draw: function() {
+      var qasmMargin = this.getElementWidth("editor");
+      var stateBtnMargin = this.getElementWidth("qubit-state");
+      var gateMargin = this.getElementWidth("circuit-gate");
+      var wireNameMargin = this.getElementWidth("qubit-name");
+      var deleteBtnMaargin = this.getElementWidth("delete-wire");
+
+      document.getElementById("circuit-block").style.height =
+        document.getElementsByClassName("circuit-wires")[0].offsetHeight +
+        2 +
+        "px";
+
+      document.getElementById("circuit-block").style.width =
+        (this.toColumn - this.fromColumn + 1) * gateMargin + "px";
+
+      document.getElementById("circuit-block").style.marginLeft =
+        this.fromColumn * gateMargin +
+        qasmMargin +
+        stateBtnMargin +
+        wireNameMargin +
+        deleteBtnMaargin +
+        "px";
+    }
+  },
+  beforeCreate() {
+    this.$nextTick(() => {
+      this.$nextTick(() => {
+        this.draw();
+      });
+    });
+  }
+  // updated: function() {
+  //   this.$nextTick(function() {
+  //     var qasmMargin = this.getElementWidth("editor");
+  //     var stateBtnMargin = this.getElementWidth("qubit-state");
+  //     var gateMargin = this.getElementWidth("circuit-gate");
+  //     var wireNameMargin = this.getElementWidth("qubit-name");
+  //     var deleteBtnMaargin = this.getElementWidth("delete-wire");
+
+  //     document.getElementById("circuit-block").style.height =
+  //       document.getElementsByClassName("circuit-wires")[0].offsetHeight +
+  //       2 +
+  //       "px";
+
+  //     document.getElementById("circuit-block").style.width =
+  //       (this.toColumn - this.fromColumn + 1) * gateMargin + "px";
+
+  //     document.getElementById("circuit-block").style.marginLeft =
+  //       this.fromColumn * gateMargin +
+  //       qasmMargin +
+  //       stateBtnMargin +
+  //       wireNameMargin +
+  //       deleteBtnMaargin +
+  //       "px";
+  //   });
+  // }
+};
+</script>
+<!-- ========================================================== -->
+<style scoped>
+#circuit-block {
+  border: 2px solid #2980b9;
+  border-radius: 0.5em;
+  /* width: 100px; */
+  /* height: 100px; */
+  z-index: -2;
+  position: absolute;
+  text-align: center;
+  margin-top: -3px;
+}
+.label {
+  position: relative;
+  top: -20px;
+  background: white;
+}
+</style>
