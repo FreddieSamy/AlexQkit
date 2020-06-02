554e43b86e8a83164e219be8f66b281ff343a1ca
mohamed hassan <sci.mohamedhassan.2017@alexu.edu.eg>
2020-06-02 21:48:05 +0200

    boolean function validation still need validation for some cases

--- client/src/components/booleanFunction.vue
+++ client/src/components/booleanFunction.vue
@@ -45,16 +45,22 @@ export default {
       - still working on it's validtion.
     */
     createBooleanCircuit() {
-      var variablesToValidate = this.variables.split(",");
-      window.console.log(variablesToValidate);
-      //var reqexToVariables=/""/
-
       var indices = document.getElementById("indices").value.split(",");
-      // window.console.log(variables);
-      //window.console.log(booleanfunction);
-      //window.console.log(indices);
-      axios
-        .post("http://127.0.0.1:5000/booleanExpression", {
+      var variablesToValidate = this.variables.split(",");
+       
+      var {
+        variablesValidate,
+        msgVar,
+        dictionaryOfVariables
+      } = this.ValidatevariableFn(this.variables);
+
+      var { ValidateExpression, msgEx } = this.ValidateExpressionFnAndIndices(
+        this.expression,
+        dictionaryOfVariables,
+        indices
+      );
+        if (variablesValidate && ValidateExpression) {
+           axios.post("http://127.0.0.1:5000/booleanExpression", {
           vars: this.variables,
           fn: this.expression,
           indices: indices
@@ -77,6 +83,14 @@ export default {
           this.$parent.$parent.setAlgorithm(algorithmObject, true);
         });
       this.closeNav();
+        }
+      else {
+        if (msgVar == "") {
+          alert(msgEx);
+        } else {
+          alert(msgVar);
+        }
+      }
     },
     openNav() {
       document.getElementById("myNav3").style.width = "100%";
@@ -84,6 +98,160 @@ export default {
       //document.getElementById("booleanfunction").value = null;
     },
     // ----------------------------------------------------
+
+     ValidatevariableFn(variables) {
+      // this.variables=this.variables.replace(/\s/g, "");
+      var variablesValidate = true;
+      var dictionaryOfVariables = {};
+      var msgVar = "";
+      var arrayofvariables;
+      var regex = /^[a-z A-Z]+([0-9]+)?$/;
+      arrayofvariables = variables.split(",");
+      window.console.log(arrayofvariables);
+
+      // check if the variables input is empty
+      var { isempty, msg } = this.inputisempty(variables);
+      if (isempty == true) {
+        msgVar = msg;
+        variablesValidate = false;
+        return { variablesValidate, msgVar, dictionaryOfVariables };
+      }
+
+      //check the value of the variable is only string and number
+      for (let indx in arrayofvariables) {
+        arrayofvariables[indx] = arrayofvariables[indx].replace(/\s/g, "");
+        variablesValidate = regex.test(arrayofvariables[indx]);
+
+        if (arrayofvariables[indx] == "") {
+          msgVar = "check if you duplicate comma (,,)";
+          return { variablesValidate, msgVar, dictionaryOfVariables };
+        } else if (variablesValidate == false) {
+          msgVar =
+            "check the value of this element " +
+            "( " +
+            arrayofvariables[indx] +
+            " )";
+          return { variablesValidate, msgVar, dictionaryOfVariables };
+        }
+      }
+      // check the dublicate of variables
+      for (let indx in arrayofvariables) {
+        if (!(arrayofvariables[indx] in dictionaryOfVariables)) {
+          dictionaryOfVariables[arrayofvariables[indx]] = 1;
+        } else {
+          msgVar = "dublicate of variables";
+          variablesValidate = false;
+          return { variablesValidate, msgVar, dictionaryOfVariables };
+        }
+      }
+
+      return { variablesValidate, msgVar, dictionaryOfVariables };
+    },
+    // ----------------------------------------------------
+     /*
+      -ValidateExpressionFnAndIndices function : used to check that the expression and indices is 
+       correct or not 
+    */
+    ValidateExpressionFnAndIndices(expression, dicvar,indices) {
+      window.console.log(dicvar);
+      var ValidateExpression = true;
+      var msgEx = "";
+
+      // check empty
+      var { isempty, msg } = this.inputisempty(expression);
+      if (isempty == true) {
+        msgEx = msg;
+        ValidateExpression = false;
+        window.console.log(ValidateExpression);
+        return { ValidateExpression, msgEx };
+      }
+
+      // to check bracket ()
+      expression = expression.replace(/\s/g, "");
+      var regex = /^([a-z A-Z]+([0-9]+)?)?\([a-z A-Z]+([0-9]+)?\)+([a-z A-Z]+([0-9]+)?)?$|^[a-z A-Z]+([0-9]+)?$/;
+      ValidateExpression = regex.test(expression);
+      if (ValidateExpression == false) {
+        msgEx = "please,check the brackets";
+        return { ValidateExpression, msgEx };
+      }
+      
+      // check the strings in the expression
+      expression = expression.split(/\s|\(|\)|or|and|not/);
+      expression = expression.filter(arr => arr); //check if the expression (array) contain empty string and delete it
+      for (let indx in expression) {
+        if (!(expression[indx] in dicvar)) {
+          ValidateExpression = false;
+          msgEx =
+            " the variable " +
+            "( " +
+            expression[indx] +
+            " )" +
+            "undefined in variales !";
+          return { ValidateExpression, msgEx };
+        } else {
+          dicvar[expression[indx]] = 0;
+        }
+      }
+      
+      // check if any variable user didn't use it in expression .
+      for (let indx in dicvar) {
+        if (dicvar[indx] == 1) {
+          ValidateExpression = false;
+          msgEx =
+            " the variable " +
+            "( " +
+            indx+
+            " )" +
+            "you didn't use it ";
+          return { ValidateExpression, msgEx };
+        }
+      }
+     
+     
+     if(ValidateExpression ==false && msg !=""){
+       return { ValidateExpression, msgEx };
+     }
+      //----
+     //check the indeces is integer or not
+     else{
+       regex=/^\d$/;
+       for(let indx in indices){
+         if(indices[indx]==""){
+           msgEx="check the indices";
+           ValidateExpression=false;
+           return{ValidateExpression, msgEx};
+         }
+
+         ValidateExpression=regex.test(indices[indx]);
+        if(ValidateExpression ==false){
+           msgEx =
+            " check  " +
+            "( " +
+            indices[indx] +
+            " )" +
+            " !";
+            return{ValidateExpression, msgEx};
+        }
+       }
+       return{ValidateExpression, msgEx};
+     }
+    },
+    // ----------------------------------------------------
+    /*
+      -inputisempty function : used to check field of input is empty or not 
+    */
+    inputisempty(valueOfInput) {
+      var isempty = false;
+      var msg = "";
+      if (valueOfInput == "" || valueOfInput.length == 0) {
+        isempty = true;
+        msg = "you have to fill variables and expression  ";
+        return { isempty, msg };
+      }
+      return { isempty, msg };
+    },
+    // ----------------------------------------------------
+
     closeNav() {
       document.getElementById("myNav3").style.width = "0%";
     }
