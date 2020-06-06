34c50fd6bb6b5f02d48508fc461d1d3603942ebf
MarioMonir <mario3monir@gmail.com>
2020-06-06 12:13:16 +0200

    Document , Optimize , Style Toolbox2

--- client/src/components/Toolbox2.vue
+++ client/src/components/Toolbox2.vue
@@ -1,47 +1,48 @@
 <template>
   <div class="toolbox-2">
-    <div class="run-reset-buttons">
-      <button @click="runCircuit">Run</button>
-      <button @click="cloneResetSystem">Reset</button>
-    </div>
+    <!-- Run and Rest Circuit Buttons -->
+    <button class="run-circuit-btn" @click="runCircuit">Run</button>
+    <button class="reset-circuit-btn" @click="cloneResetSystem">Reset</button>
+    <!-- end  Run and Rest Circuit Buttons -->
 
     <!-- Tracing Buttons -->
-    <div class="tracing-buttons">
-      <button class="exeBtn" @click="exeStart">
-        <img src="../assets/icons8-skip-to-start-48.png" alt />
-      </button>
-      <button class="exeBtn" @click="preExe">
-        <img src="../assets/icons8-previous-26.png" alt />
-      </button>
-      <button class="exeBtn" @click="nextExe">
-        <img src="../assets/icons8-next-26.png" alt />
-      </button>
-      <button class="exeBtn" @click="exeEnd">
-        <img src="../assets/icons8-end-48.png" alt />
-      </button>
-    </div>
+    <button class="exe-btn" @click="exeStart">
+      <img src="../assets/icons8-skip-to-start-48.png" alt />
+    </button>
+    <button class="exe-btn" @click="preExe">
+      <img src="../assets/icons8-previous-26.png" alt />
+    </button>
+    <button class="exe-btn" @click="nextExe">
+      <img src="../assets/icons8-next-26.png" alt />
+    </button>
+    <button class="exe-btn" @click="exeEnd">
+      <img src="../assets/icons8-end-48.png" alt />
+    </button>
     <!-- end Tracing buttons -->
 
-    <div class="elementary-gates">
-      <button class="exeBtn" @click="this.$parent.elementaryGates">Elementary Gates</button>
-    </div>
+    <!-- Elementary Gates Button -->
+    <button class="elementary-gates" @click="this.$parent.elementaryGates">Elementary Gates</button>
+    <!-- end Elementary Gates Button -->
 
+    <!-- Boolean Function Button -->
     <booleanFunction ref="createBoolean" class="boolean-function" />
+    <!-- end  Boolean Function Button -->
 
-    <div class="circiut-loop">
-      <Circiutloops :colsCount="this.jsonObject.colsCount" />
-    </div>
+    <!-- Circuit loops Button -->
+    <Circiutloops class="circuit-loops" :colsCount="this.jsonObject.colsCount" />
+    <!-- end Circuit loops Button -->
 
-    <div class="save-circuit">
-      <input type="text" placeholder="Save Circuit by Name" v-model="savedCirciutName" />
-      <button @click="saveCircuit(savedCirciutName)">Save Circuit</button>
-    </div>
-    <div class="select-algorithm">
-      <label>Select an Algorithm</label>
-      <select v-model="selectedAlgorithm">
-        <option v-for="(item, index) in algorithms" :key="index" :value="item">{{ item.name }}</option>
-      </select>
-    </div>
+    <!-- Save Circuit Block -->
+    <input type="text" placeholder="Save Circuit by Name" v-model="savedCirciutName" />
+    <button class="save-circuit-btn" @click="saveCircuit(savedCirciutName)">Save Circuit</button>
+    <!-- Save Circuit Block -->
+
+    <!-- Select Algorithm Block -->
+    <label>Select an Algorithm</label>
+    <select v-model="selectedAlgorithm">
+      <option v-for="(item, index) in algorithms" :key="index" :value="item">{{ item.name }}</option>
+    </select>
+    <!-- end Select Algorithm Block -->
   </div>
 </template>
 <!-- =============================================================  -->
@@ -60,24 +61,27 @@ export default {
   props: ["setAlgorithm"],
   data() {
     return {
-      selectedAlgorithm: null,
+      selectedAlgorithm: null, 
       savedCirciutName: ""
     };
   },
   watch: {
+    // this watcher watch if you select algorithm from the dropdown list of algorithms
+    // and if you choose algorithm it apply it on circuit by-> setAlgotrithm
     selectedAlgorithm() {
       if (this.selectedAlgorithm != null) {
-        //window.console.log(this.selectedAlgorithm)
         this.setAlgorithm(this.selectedAlgorithm);
         this.selectedAlgorithm = null;
       }
     }
   },
   mounted() {
+    // when mount the componenet check for saved algorithms in local storage
+    // of the browser to get it 
     if (localStorage.hasOwnProperty("algorithms")) {
       this.getLocal("algorithms");
     } else {
-      // in case of first run time only
+      // in case of first time to run  on browser or (empty local Storage )
       this.storeLocal("algorithms");
     }
   },
@@ -93,10 +97,11 @@ export default {
     ...mapActions(["storeLocal"]),
     ...mapActions(["getLocal"]),
     cloneResetSystem: function() {
-      this.selectedAlgorithm = null;
+      // just reset the system by the function in clone Component
       this.$parent.resetSystem();
     },
     nextExe: function() {
+      // Step forward for the Tracing line and run circuit 
       if (this.jsonObject.exeCount < this.jsonObject.colsCount) {
         this.jsonObject.exeCount++;
         this.$parent.$refs.tracingLine.updateTracingLine();
@@ -105,6 +110,7 @@ export default {
     },
     //-----------------------------------------------------------------------
     preExe: function() {
+      // Step Backward for the Tracing line and run circuit 
       if (this.jsonObject.exeCount > 0) {
         this.jsonObject.exeCount--;
         this.$parent.$refs.tracingLine.updateTracingLine();
@@ -112,6 +118,7 @@ export default {
       }
     },
     //-----------------------------------------------------------------------
+    // Skip to Start for the Tracing line and run circuit 
     exeStart: function() {
       if (this.jsonObject.exeCount != 0) {
         this.jsonObject.exeCount = 0;
@@ -120,6 +127,7 @@ export default {
       }
     },
     //-----------------------------------------------------------------------
+    // Skip to end for the Tracing line and run circuit 
     exeEnd: function() {
       if (this.jsonObject.exeCount != this.jsonObject.colsCount) {
         this.jsonObject.exeCount = this.jsonObject.colsCount;
@@ -127,6 +135,8 @@ export default {
         this.runCircuit();
       }
     },
+    // Save The Circuit applied after counting it's special gates
+    // then store it in the local storage  
     saveCircuit(name) {
       if (name != "") {
         if (this.jsonObject.colsCount) {
@@ -140,10 +150,7 @@ export default {
               swaps: this.countGate("Swap")
             }
           });
-          window.console.log("store");
-          window.console.log(this.algorithms);
           this.storeLocal("algorithms");
-
           this.savedCirciutName = "";
         } else {
           alert("empty circuit !!");
@@ -159,20 +166,16 @@ export default {
 <style scoped>
 .toolbox-2 {
   display: flex;
-  flex-direction: row;
   justify-content: flex-start;
   align-items: center;
   flex-wrap: wrap;
-  padding: 0px 50px 0px 5px;
-  margin: 0px;
+  padding: 0px 50px;
 }
 button {
-  display: inline-block;
-  margin: 0px 10px;
- 
   background-color: white;
   border-radius: 0.5em;
   border: 2px solid grey;
+  margin:0px 10px;
 }
 input {
   border-radius: 7px;
@@ -181,36 +184,23 @@ select {
   border-radius: 7px;
   background: white;
 }
-img{
+img {
   width: 12px;
   height: 10px;
 }
-.run-reset-buttons {
-  flex-basis: 5%;
+.run-circuit-btn{
+ margin:0px 0px 0px 10px;
 }
-.tracing-buttons {
-  flex-basis: 5%;
+.reset-circuit-btn{
+  margin:0px 20px 0px 0px;
 }
-.tracing-buttons button {
-  margin:0px
+.exe-btn{
+  margin:0;
 }
-.boolean-function {
-  flex-basis: 5%;
-  /* margin: 0px 15px 0px 0px; */
+.save-circuit-btn{
+  margin: 0px 30px 0px 0px;
 }
-.elementary-gates {
-  flex-basis: 5%;
-}
-.circiut-loop {
-  flex-basis: 5%;
-  margin: 0px 15px 0px 0px;
-}
-.save-circuit {
-  flex-basis: 5%;
-  margin: 0px 10px 0px 10px;
-}
-.select-algorithm {
-  flex-basis: 5%;
-  margin: 0px 10px 0px 10px;
+.circuit-loops{
+  margin:0px 30px 0px 10px;
 }
 </style>
